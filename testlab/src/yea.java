a7c7f6e3f6eda70afe59fa64732dbf401d853a0a
novakc <novakc@iu.edu>
2021-08-27 12:49:56 -0400

    RandomDNA

---
+++ testlab/src/yea.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+public class yea {
+
+    public static void main(String args[])
+    {
+        String[] dnaStrand = {"A", "T", "C", "G"};
+        String result = "";
+        Random rnd = new Random();
+
+        for (int i = 0; i < 20; i++)
+        {
+            int r = rnd.nextInt(4);
+            result += dnaStrand[r];
+        }
+        System.out.println(result);
+    }
+}
